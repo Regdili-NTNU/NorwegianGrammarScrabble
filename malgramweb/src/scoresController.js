a1b9e78b3a9213b558eb1561b524bc48c0f7a325
Elias Aamot <elias.aamot@gmail.com>
2016-04-30 00:25:01 +0200

    Introduces rudimentary l10n for in-site text.

--- malgramweb/src/scoresController.js
+++ malgramweb/src/scoresController.js
@@ -1,4 +1,4 @@
-app.controller("scoresController", function($scope, $http, $mdDialog) {
+app.controller("scoresController", function($scope, $http, $mdDialog, $window) {
 	$scope.scores = [];
 
 	$scope.getScores = function() {
@@ -11,4 +11,41 @@ app.controller("scoresController", function($scope, $http, $mdDialog) {
 		};
 
 	$scope.getScores();
+
+	/**
+	 * I18n functions
+	 */
+
+	$scope.getLanguage = function() {
+		var languageString = $window.navigator.language;
+		if (languageString.indexOf('no') >= 0) {
+			return "no";
+		}
+		return "en";
+	};
+
+	$scope.high_score_string = function() {
+		var language = $scope.getLanguage();
+		if (language == "no") {
+			return "Poengtavle"; 
+		}
+		return "High score";
+	}
+
+	$scope.no_score_string = function() {
+		var language = $scope.getLanguage();
+		if (language == "no") {
+			return "Ingen har ennå registert en poengsum. Spill en runde for å bli den første!"; 
+		}
+		return "No scores have been registered yet. Play a round to be the first!";
+	}
+
+	$scope.start_new_game_string = function() {
+		var language = $scope.getLanguage();
+		if (language == "no") {
+			return "Start ny runde"; 
+		}
+		return "Start new game";
+	}
+
 });
